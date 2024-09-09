d03b9271d39d5312e56f4a9afb18a896b8741c22
victor <103121630+victor-914@users.noreply.github.com>
2024-09-09 03:17:13 +0100

    news_page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ["ec2-13-51-65-133.eu-north-1.compute.amazonaws.com"], // Add your domain here
+  },
 };
 
 export default nextConfig;
+
+
+
+
